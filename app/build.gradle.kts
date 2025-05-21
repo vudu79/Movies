09e767719a3361c61137f3e5600b1c5c918615d4
Andrey <vudu1979@gmail.com>
2025-05-21 18:16:41 +0200

    add posters, menu buttons, styles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.androidx.appcompat.v120)
+    implementation (libs.androidx.core.ktx.v131)
 }
