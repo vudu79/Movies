e7624a667991191abbc6f926d815bf99dc21f4e7
Andrey <vudu1979@gmail.com>
2025-05-30 22:44:22 +0200

    refactor FavoriteFragment

--- app/src/main/java/ru/vodolatskii/movies/presentation/fragments/FavoriteFragment.kt
+++ app/src/main/java/ru/vodolatskii/movies/presentation/fragments/FavoriteFragment.kt
@@ -1,14 +1,15 @@
-package ru.vodolatskii.movies
+package ru.vodolatskii.movies.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ru.vodolatskii.movies.databinding.FragmentFavoriteBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 
 class FavoriteFragment : Fragment() {
     private lateinit var binding: FragmentFavoriteBinding
