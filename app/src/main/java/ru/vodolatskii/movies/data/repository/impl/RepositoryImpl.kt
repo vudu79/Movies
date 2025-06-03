b8b13e0974f30db79e6ec8cf57b462a1baab1ce6
Andrey <vudu1979@gmail.com>
2025-06-03 13:27:39 +0200

    refactor

--- app/src/main/java/ru/vodolatskii/movies/data/repository/impl/RepositoryImpl.kt
+++ app/src/main/java/ru/vodolatskii/movies/data/repository/impl/RepositoryImpl.kt
@@ -3,7 +3,6 @@ package ru.vodolatskii.movies.data.repository.impl
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import ru.vodolatskii.movies.App
@@ -98,30 +97,6 @@ class RepositoryImpl() : Repository {
 
     override suspend fun getAllMoviesFromFavorites(): List<Movie>? {
         return movieDao.getAllMovie()
-//        return listOf(
-//
-//            Movie(
-//                id = 3877L,
-//                name = "Jameelah",
-//                description = "Erendira",
-//                posterUrl = "https://image.openmoviedb.com/kinopoisk-images/10900341/caf9f155-1a19-42f1-a0f3-9c8773e9083e/orig",
-//                isFavorite = true
-//            ),
-//            Movie(
-//                id = 8063L,
-//                name = "Malka",
-//                description = "Millard",
-//                posterUrl = "https://image.openmoviedb.com/kinopoisk-images/1599028/637271d5-61b4-4e46-ac83-6d07494c7645/orig",
-//                isFavorite = true
-//            ),
-//            Movie(
-//                id = 3363L,
-//                name = "sdfsdfsdf",
-//                description = "Milsdsdfsdfsdfsdfsdfslard",
-//                posterUrl = "https://image.openmoviedb.com/kinopoisk-images/1898899/5fb7d956-d5fb-4189-9ec9-1a051aaa7f41/orig",
-//                isFavorite = true
-//            )
-//        )
     }
 }
 
