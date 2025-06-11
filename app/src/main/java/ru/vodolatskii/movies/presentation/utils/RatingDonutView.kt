7b0b8f922f10f0b6d0a2529d3b4c736ea95a50ee
Andrey <vudu1979@gmail.com>
2025-06-11 19:51:41 +0200

    refactor

--- app/src/main/java/ru/vodolatskii/movies/presentation/utils/RatingDonutView.kt
+++ app/src/main/java/ru/vodolatskii/movies/presentation/utils/RatingDonutView.kt
@@ -15,26 +15,19 @@ class RatingDonutView @JvmOverloads constructor(
     attributeSet: AttributeSet? = null
 ) : View(context, attributeSet) {
 
-    //Овал для рисования сегментов прогресс бара
     private val oval = RectF()
-    //Координаты центра View, а также Radius
     private var radius: Float = 0f
     private var centerX: Float = 0f
     private var centerY: Float = 0f
-    //Толщина линии прогресса
     private var stroke = 10f
-    //Значение прогресса от 0 - 100
     private var progress = 50
-    //Значения размера текста внутри кольца
     private var scaleSize = 30f
-    //Краски для наших фигур
     private lateinit var strokePaint: Paint
     private lateinit var digitPaint: Paint
     private lateinit var circlePaint: Paint
 
 
     init {
-        //Получаем атрибуты и устанавливаем их в соответствующие поля
         val a =
             context.theme.obtainStyledAttributes(attributeSet, R.styleable.RatingDonutView, 0, 0)
         try {
@@ -44,7 +37,6 @@ class RatingDonutView @JvmOverloads constructor(
         } finally {
             a.recycle()
         }
-        //Инициализируем первоначальные краски
         initPaint()
     }
 
@@ -74,9 +66,7 @@ class RatingDonutView @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas) {
-        //Рисуем кольцо и задний фон
         drawRating(canvas)
-        //Рисуем цифры
         drawText(canvas)
     }
 
@@ -88,50 +78,34 @@ class RatingDonutView @JvmOverloads constructor(
 
 
     private fun drawRating(canvas: Canvas) {
-        //Здесь мы можем регулировать размер нашего кольца
         val scale = radius * 0.8f
-        //Сохраняем канвас
         canvas.save()
-        //Перемещаем нулевые координаты канваса в центр, вы помните, так проще рисовать все круглое
         canvas.translate(centerX, centerY)
-        //Устанавливаем размеры под наш овал
         oval.set(0f - scale, 0f - scale, scale , scale)
-        //Рисуем задний фон(Желательно его отрисовать один раз в bitmap, так как он статичный)
         canvas.drawCircle(0f, 0f, radius, circlePaint)
-        //Рисуем "арки", из них и будет состоять наше кольцо + у нас тут специальный метод
         canvas.drawArc(oval, -90f, convertProgressToDegrees(progress), false, strokePaint)
-        //Восстанавливаем канвас
         canvas.restore()
     }
 
     private fun convertProgressToDegrees(progress: Int): Float = progress * 3.6f
 
     private fun drawText(canvas: Canvas) {
-        //Форматируем текст, чтобы мы выводили дробное число с одной цифрой после точки
         val message = String.format("%.1f", progress / 10f)
-        //Получаем ширину и высоту текста, чтобы компенсировать их при отрисовке, чтобы текст был
-        //точно в центре
         val widths = FloatArray(message.length)
         digitPaint.getTextWidths(message, widths)
         var advance = 0f
         for (width in widths) advance += width
-        //Рисуем наш текст
         canvas.drawText(message, centerX - advance / 2, centerY  + advance / 4, digitPaint)
     }
 
 
     private fun initPaint() {
-        //Краска для колец
         strokePaint = Paint().apply {
             style = Paint.Style.STROKE
-            //Сюда кладем значение из поля класса, потому как у нас краски будут видоизменяться
             strokeWidth = stroke
-            //Цвет мы тоже будем получать в специальном методе, потому что в зависимости от рейтинга
-            //мы будем менять цвет нашего кольца
             color = getPaintColor(progress)
             isAntiAlias = true
         }
-        //Краска для цифр
         digitPaint = Paint().apply {
             style = Paint.Style.FILL_AND_STROKE
             strokeWidth = 2f
@@ -141,7 +115,6 @@ class RatingDonutView @JvmOverloads constructor(
             color = getPaintColor(progress)
             isAntiAlias = true
         }
-        //Краска для заднего фона
         circlePaint = Paint().apply {
             style = Paint.Style.FILL
             color = Color.DKGRAY
@@ -155,11 +128,8 @@ class RatingDonutView @JvmOverloads constructor(
     }
 
     fun setProgress(pr: Int) {
-        //Кладем новое значение в наше поле класса
         progress = pr
-        //Создаем краски с новыми цветами
         initPaint()
-        //вызываем перерисовку View
         invalidate()
     }
 }
