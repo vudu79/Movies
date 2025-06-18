58bf5f96c77c01f79560147a3bf887ac5e38a349
Andrey <vudu1979@gmail.com>
2025-06-18 11:53:51 +0200

    for pull request

--- app/src/main/java/ru/vodolatskii/movies/data/service/TMDBApiService.kt
+++ app/src/main/java/ru/vodolatskii/movies/data/service/TMDBApiService.kt
@@ -7,6 +7,7 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 import ru.vodolatskii.movies.data.entity.dto.TMDBPopularMoviesRespDto
 
+
 interface TMDBsApiService {
     @Headers(
         "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzMyNDUyOTlhMDBiNGNmZTU5MzdhZGM5MDRkZGQwYiIsIm5iZiI6MTc0Nzk0NDU4MS4xMTIsInN1YiI6IjY4MmY4NDg1NjM2ODcwMmEyMWI2YTUxYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gqqroOOQUKINr-EuwLXLUVZpw-rj3VuzGeb08dtwjec",
