3df39792937df59353e4720634e2a950f535d56c
Andrey <vudu1979@gmail.com>
2025-06-18 17:15:43 +0200

    8

--- app/src/main/java/ru/vodolatskii/movies/common/Constant.kt
+++ app/src/main/java/ru/vodolatskii/movies/common/Constant.kt
@@ -7,4 +7,7 @@ class Constant {
         const val API_KEY_TMDB =
             "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzMyNDUyOTlhMDBiNGNmZTU5MzdhZGM5MDRkZGQwYiIsIm5iZiI6MTc0Nzk0NDU4MS4xMTIsInN1YiI6IjY4MmY4NDg1NjM2ODcwMmEyMWI2YTUxYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gqqroOOQUKINr-EuwLXLUVZpw-rj3VuzGeb08dtwjec"
     }
+
+
+
 }
